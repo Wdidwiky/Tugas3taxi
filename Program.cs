1bc6ec88241793419d535f7c70b1b80093d46197
wdidwiky <101820999+Wdidwiky@users.noreply.github.com>
2022-03-23 20:46:02 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Taxi taxi = new Taxi();
+
+        taxi.DriverName = "Jono";
+        taxi.Onduty = true;
+        taxi.NumPassenger = 10;
+
+        taxi.TaxiInfo();
+        taxi.PickUpPassenger();
+        taxi.DropOffPassenger();
+
+        Console.ReadKey();
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public int NumPassenger {get; set; }
+        public bool Onduty { get; set; }
+
+        public void TaxiInfo()
+        {
+            string duty;
+            if(Onduty)
+            {
+                duty = "yes";
+            }else
+            {
+                duty = "no";
+            }
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", Onduty);
+            Console.WriteLine("Number Of Passenger : {0}", NumPassenger);
+            Console.WriteLine();
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Selesai mengantar penumpang", DriverName);
+        }
+    }
